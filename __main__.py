d13e899da87193ca26fa35ac26acfc6e42843090
Jerónimo Luza <jero.luza@gmail.com>
2025-05-08 16:08:32 -0300

    moved run to local dev __main__

--- __main__.py
+++ __main__.py
@@ -1,26 +1,32 @@
 from shapely import wkt
+import geopandas as gpd
 
 if __name__ == "__main__":
 
     from nightlights import download, plotting, process
 
-    session_slug = "main"
-    download_dir = f"./data/{session_slug}/raw"
-    extraction_dir = f"./data/{session_slug}/extracted"
-    plot_dir = f"./data/{session_slug}/plots"
-    final_output_dir = f"./data/{session_slug}/output"
+    download_dir = "./data/raw"
+    extraction_dir = "./data/extracted"
+    plot_dir = "./data/plots"
+    final_output_dir = "./data/output"
 
     # Define search parameters
-    short_name = "VNP46A2"
-    version = "2"
-    start_date = "2025-04-20"
-    end_date = "2025-05-5"
+    short_name = "VNP46A3"
+    version = "1"
+    start_date = "2023-09-20"
+    end_date = "2024-01-20"
 
-    region = wkt.loads(
-        # "POLYGON((33.0 14.89, 47.98 14.89, 47.98 3.4, 33.0 3.4, 33.0 14.89))"
-        # "POLYGON((34.8826 34.6924, 36.625 34.6924, 36.625 33.055, 34.8826 33.055, 34.8826 34.6924))"
-        "POLYGON((-4.71 41.04, -2.63 41.04, -2.63 39.71, -4.71 39.71, -4.71 41.04))"
+    # region = wkt.loads(
+    #     # "POLYGON((33.0 14.89, 47.98 14.89, 47.98 3.4, 33.0 3.4, 33.0 14.89))"
+    #     # "POLYGON((34.8826 34.6924, 36.625 34.6924, 36.625 33.055, 34.8826 33.055, 34.8826 34.6924))"
+    #     "POLYGON((-4.71 41.04, -2.63 41.04, -2.63 39.71, -4.71 39.71, -4.71 41.04))"
+    # )
+
+    gdf = gpd.read_file(
+        "https://raw.githubusercontent.com/datasets/geo-countries/main/data/countries.geojson"
     )
+    region_crs = gdf.crs.to_epsg()
+    region = gdf[gdf.name == "Lebanon"].geometry.values[0]
 
     files = download.download_earthaccess(
         download_dir=download_dir,
@@ -31,8 +37,8 @@ if __name__ == "__main__":
         region=region,
     )
 
-    # variable_name = "AllAngle_Composite_Snow_Free"
-    variable_name = "DNB_BRDF-Corrected_NTL"
+    variable_name = "AllAngle_Composite_Snow_Free"
+    # variable_name = "DNB_BRDF-Corrected_NTL"
 
     # process.process_files(files, variable_name=variable_name, output_dir=extraction_dir, bounding_box=region.bounds)
 
@@ -46,12 +52,13 @@ if __name__ == "__main__":
     #     region=region,
     # )
 
-    plotting.static.create_timelapse_gif(
+    plotting.create_timelapse_gif(
         files,
         variable_name=variable_name,
         title="Lebanon Blackouts\nMonthly Nightlights",
         output_dir=plot_dir,
         region=region,
+        region_crs=region_crs,
         fps=2.0,
         plot_series=True,
     )
@@ -59,7 +66,3 @@ if __name__ == "__main__":
     # gdf = process.process_files(
     #     files, variable_name=variable_name, bounding_box=region.bounds
     # )
-    # plotting.interactive.create_folium_map(
-    #     gdf, variable_name=variable_name, output_file="test.html"
-    # )
-    #
--- nightlights/plotting/__init__.py
+++
@@ -1,9 +0,0 @@
-from nightlights.plotting import static
-from nightlights.plotting import interactive
-
-# Import functions from static.py to make them available at the package level
-from nightlights.plotting.static import (
-    plot_file,
-    plot_all_files,
-    create_timelapse_gif
-)
